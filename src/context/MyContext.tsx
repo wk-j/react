c9a90f6088fa5df124c8d6118128fbb65b145371
wk-j <somnuk.wk@gmail.com>
2019-04-09 17:10:21 +0700

    Context

--- src/context/MyContext.tsx
+++ src/context/MyContext.tsx
@@ -7,10 +7,66 @@ type Data = {
 
 const { Provider, Consumer } = React.createContext<Data>({ a: 100, b: 200 });
 
-export class MyContext extends React.Component {
+type State = {
+    data: Data
+}
+
+export class MyContext extends React.Component<{}, State> {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: { a: 100, b: 200 }
+        }
+    }
+
+    click = (e) => {
+        this.setState({
+            data: { ... this.state.data, a: this.state.data.a + 1 }
+        })
+    }
+
     render() {
         return (
-            <h1>Context</h1>
+            <div>
+                <Provider value={this.state.data}>
+                    <h1 onClick={this.click}>Provider {this.state.data.a}</h1>
+                    <Comp1 />
+                </Provider>
+            </div>
+        )
+    }
+}
+
+class Comp1 extends React.Component<{}, {}> {
+    render() {
+        return (
+            <Consumer>
+                {
+                    ({ a, b }) =>
+                        <div>
+                            <div>{a}</div>
+                            <div>{b}</div>
+                            <Comp2 />
+                        </div>
+                }
+            </Consumer>
+        )
+    }
+}
+
+class Comp2 extends React.Component {
+    render() {
+        return (
+            <Consumer>
+                {
+                    ({ a, b }) =>
+                        <div>
+                            <div>{a}</div>
+                            <div>{b}</div>
+                        </div>
+                }
+            </Consumer>
         )
     }
 }
