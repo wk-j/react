5d6db515b47c97f4909e917b44b065c70d497214
wk-j <somnuk.wk@gmail.com>
2020-02-23 22:03:27 +0700

    Error

--- src/error/MyErrorBoundary.tsx
+++ src/error/MyErrorBoundary.tsx
@@ -1,33 +1,56 @@
 import React from "react"
 
 type State = {
-    error: string
+    hasError: boolean
 }
 
-export class MyErrorBoundary extends React.Component<{}, State> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            error: ""
-        }
-    }
-    componentDidCatch(error, info) {
-        this.setState({
-            error: error
-        })
-    }
+
+
+class MyWidget extends React.Component {
 
     click = (e) => {
         throw new Error('I crashed!');
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Error</h1>
-                <div onClick={this.click}>Click</div>
-                <h1>{this.state.error}</h1>
-            </div>
-        )
+    render = () =>
+        <div>
+            <h1>Error</h1>
+            <div onClick={this.click}>Click</div>
+        </div>
+
+}
+
+
+class ErrorBoundary extends React.Component<{}, State> {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
     }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        // logErrorToMyService(error, errorInfo);
+        console.log(error)
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export class MyErrorBoundary extends React.Component {
+    render = () =>
+        <ErrorBoundary>
+            <MyWidget></MyWidget>
+        </ErrorBoundary>
 }
